0663806cb71f40b5387b4ab37d0fb419777f05ff
nishant184 <pardeshinishant57@gmail.com>
2020-11-28 14:46:39 +0530

    the website is created

---
+++ forms.py
@@ -0,0 +1,11 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField,IntegerField,SubmitField
+
+class AddForm(FlaskForm):
+    name = StringField("Name of puppy:")
+    submit = SubmitField('Add Puppy')
+
+class DelForm(FlaskForm):
+    id = IntegerField("Id number of puppy to remove")
+    submit = SubmitField("Remove puppy")
+    
